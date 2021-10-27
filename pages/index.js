7c79629447ef9a902ed3fe30b616affb780d2469
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2021-10-27 10:13:20 +0200

    Added layout and header

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Web Dev Newz</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>welcome to Next</h1>
+      <h1>Welcome to Next</h1>
     </div>
   );
 }
